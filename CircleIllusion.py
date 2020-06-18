1b5a6c97a4bf15e226da256801966a1cc94204d9
Shane McDonough <shanemcdman@gmail.com>
2020-06-18 16:40:32 -0700

    Finish track

--- CircleIllusion.py
+++ CircleIllusion.py
@@ -6,12 +6,14 @@ class CircleIllusion:
     Draw a circle created out of balls moving on seperate tracks using pygame
     """
 
-    def __init__(self, width, height):
+    def __init__(self, width, height, speed = 0.05):
         self.size = Point(width, height)
+        self.speed = speed
         self.running = True
         pygame.display.init()
         pygame.display.set_caption("Circle Illusion")
         self.screen = pygame.display.set_mode(self.size)
+        self.track = Track(self.screen, Point(30, 300), Point(570, 300), 0)
 
     def run(self):
         while self.running:
@@ -19,4 +21,6 @@ class CircleIllusion:
                 if event.type == pygame.QUIT:
                     self.running = False
             self.screen.fill((0, 0, 0))
+            self.track.draw()
+            self.track.update(self.speed)
             pygame.display.update()
--- Track.py
+++ Track.py
@@ -5,8 +5,30 @@ Point = collections.namedtuple("Point", ['x', 'y'])
 
 class Track:
 
-    def __init__(self, screen: pygame.Surface, start: Point, end: Point, dist_from_start: float):
+    def __init__(self, screen: pygame.Surface, start: Point, end: Point, percentage_from_start: float):
         self.screen = screen
         self.start = start
         self.end = end
-        self.dist_from_start = dist_from_start
+        self.percentage_from_start = percentage_from_start
+        self.increasing = True
+
+    def draw(self) -> None:
+        pygame.draw.circle(self.screen, (255, 255, 255), self.start, 3)
+        pygame.draw.circle(self.screen, (255, 255, 255), self.end, 3)
+        pygame.draw.line(self.screen, (255, 255, 255), self.start, self.end)
+        dec = self.percentage_from_start / 100
+        pos = Point(
+                int((1 - dec) * self.start.x + dec * self.end.x),
+                int((1 - dec) * self.start.y + dec * self.end.y)
+                )
+        pygame.draw.circle(self.screen, (255, 0, 0), pos,10)
+
+    def update(self, speed: float) -> None:
+        if self.increasing:
+            self.percentage_from_start += speed
+            if self.percentage_from_start >= 100:
+                self.increasing = False
+        else:
+            self.percentage_from_start -= speed
+            if self.percentage_from_start <= 0:
+                self.increasing = True
