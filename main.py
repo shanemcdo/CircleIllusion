0007de395eeeeaddc0fe1e84acc6f6e92c439652
Shane McDonough <shanemcdman@gmail.com>
2020-06-19 12:27:51 -0700

    Call print_controls

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from CircleIllusion import CircleIllusion
 
 def main():
     os.environ["SDL_VIDEO_WINDOW_POS"] = "15,45"
+    CircleIllusion.print_controls()
     c = CircleIllusion(600, 600)
     c.run()
 
