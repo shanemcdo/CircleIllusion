783c7dca6b6dad17f201a57404a8ec1df766ffc5
Shane McDonough <shanemcdman@gmail.com>
2020-06-18 19:24:58 -0700

    Create kbin

--- CircleIllusion.py
+++ CircleIllusion.py
@@ -14,8 +14,10 @@ class CircleIllusion:
         self.running = True
         pygame.display.init()
         pygame.display.set_caption("Circle Illusion")
+        pygame.key.set_repeat(40)
         self.screen = pygame.display.set_mode(self.size)
-        self.create_tracks(15)
+        self.number_of_tracks = 15
+        self.create_tracks(self.number_of_tracks)
 
     def create_tracks(self, num: int) -> None:
         self.tracks = []
@@ -29,8 +31,20 @@ class CircleIllusion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                elif event.type == pygame.KEYDOWN:
+                    self.kbin(event.unicode)
             self.screen.fill((0, 0, 0))
             for track in self.tracks:
                 track.draw()
                 track.update(self.speed)
             pygame.display.update()
+
+    def kbin(self, key: str) -> None:
+        if key == '-':
+            self.number_of_tracks -= 1
+            if self.number_of_tracks < 1:
+                self.number_of_tracks = 1
+            self.create_tracks(self.number_of_tracks)
+        elif key == '+':
+            self.number_of_tracks += 1
+            self.create_tracks(self.number_of_tracks)
